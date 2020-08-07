b13bbe5b3e3d29bf1181202eaa1bfb3b81c126c1
lzkids <kidsde1314@gmail.com>
2020-08-07 14:14:24 +0800

    导入el组件库

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+// 引入EelUi组件库及样式
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
-
+// 注册
+Vue.use(ElementUI)
 new Vue({
   router,
   render: h => h(App)
